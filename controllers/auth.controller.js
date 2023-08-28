bc7b080d733052c977c4c7b7ac127dd65aedf0cf
Ajinkya710 <ajinkyabuva7@gmail.com>
2023-08-28 15:32:45 +1000

    modified node.js app

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,80 +1,65 @@
-var jwt = require("jsonwebtoken");
-var bcrypt = require("bcrypt");
-var User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
 
-exports.signup = (req, res) => {
-  const user = new User({
-    fullName: req.body.fullName,
-    email: req.body.email,
-    role: req.body.role,
-    password: bcrypt.hashSync(req.body.password, 8)
-  });
+exports.signup = async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 8);
 
-  user.save((err, user) => {
-    if (err) {
-      res.status(500)
-        .send({
-          message: err
-        });
-      return;
-    } else {
-      res.status(200)
-        .send({
-          message: "User Registered successfully"
-        })
-    }
-  });
+    const user = new User({
+      fullName: req.body.fullName,
+      email: req.body.email,
+      role: req.body.role,
+      password: hashedPassword
+    });
+
+    await user.save();
+
+    res.status(200).send({
+      message: "User Registered successfully"
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || "An error occurred while registering the user."
+    });
+  }
 };
 
-exports.signin = (req, res) => {
-  User.findOne({
-      email: req.body.email
-    })
-    .exec((err, user) => {
-      if (err) {
-        res.status(500)
-          .send({
-            message: err
-          });
-        return;
-      }
-      if (!user) {
-        return res.status(404)
-          .send({
-            message: "User Not found."
-          });
-      }
+exports.signin = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
 
-      //comparing passwords
-      var passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
-      // checking if password was valid and send response accordingly
-      if (!passwordIsValid) {
-        return res.status(401)
-          .send({
-            accessToken: null,
-            message: "Invalid Password!"
-          });
-      }
-      //signing token with user id
-      var token = jwt.sign({
-        id: user.id
-      }, process.env.API_SECRET, {
-        expiresIn: 86400
+    if (!user) {
+      return res.status(404).send({
+        message: "User Not found."
       });
+    }
 
-      //responding to client request with user profile success message and  access token .
-      res.status(200)
-        .send({
-          user: {
-            id: user._id,
-            email: user.email,
-            fullName: user.fullName,
-          },
-          message: "Login successfull",
-          accessToken: token,
-        });
+    const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: "Invalid Password!"
+      });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.API_SECRET, {
+      expiresIn: 86400
     });
+
+    res.status(200).send({
+      user: {
+        id: user._id,
+        email: user.email,
+        fullName: user.fullName
+      },
+      message: "Login successful",
+      accessToken: token
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || "An error occurred while signing in."
+    });
+  }
 };
